7f7b20961d99fa3ccc9599926d82885f922fec5c
Trushal-Talhah <paltrushal150@gmail.com>
2026-03-14 08:57:35 +0530

    Fix: convert public from submodule to normal folder

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "smarthelmet-v2";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
